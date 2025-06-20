539166f73269692bd1e21fe0a7655e48f96ffd24
DUDULRX <162013353+DUDULRX@users.noreply.github.com>
2025-06-20 17:18:07 +0800

    Update utils.mjs

--- roarm-sdk/utils.mjs
+++ roarm-sdk/utils.mjs
@@ -170,10 +170,20 @@ export class PortHandler {
     try {
       await this.port.open({ 
         baudRate: this.baudrate,  
-        dataTerminalReady: false,
-        requestToSend: false 
+        // dataTerminalReady: false,
+        // requestToSend: false 
       });
-      await this.port.setSignals({ dataTerminalReady: false, requestToSend: false });
+      // await this.port.setSignals({ dataTerminalReady: false, requestToSend: false });
+      const signals = await this.port.getSignals();
+      console.log(`Clear To Send:       ${signals.clearToSend}`);
+      console.log(`Data Carrier Detect: ${signals.dataCarrierDetect}`);
+      console.log(`Data Set Ready:      ${signals.dataSetReady}`);
+      console.log(`Ring Indicator:      ${signals.ringIndicator}`);   
+
+      // Turn on Data Terminal Ready (DTR) signal.
+      await this.port.setSignals({ dataTerminalReady: true });
+      // Turn off Request To Send (RTS) signal.
+      await this.port.setSignals({ requestToSend: false });
       this.reader = this.port.readable.getReader();
       this.writer = this.port.writable.getWriter();
       this.isOpen = true;
